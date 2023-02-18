e71f0f894bdce8aa1430614da53ebe369ada043d
Thalisson-Alves <thalisson2030@gmail.com>
2023-02-18 20:45:55 -0300

    Fix Scrapping; saving html

--- src/utils/scrappers/sigaa_scrapper.py
+++ src/utils/scrappers/sigaa_scrapper.py
@@ -1,5 +1,6 @@
 import os
 import re
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,13 +10,16 @@ from utils.entities.discipline import Discipline
 from utils.entities.offer import Offer
 from utils.entities.schedule import Schedule, Time
 
-CURRENT_YEAR = os.getenv('CURRENT_YEAR', 2022)
-CURRENT_PERIOD = os.getenv('CURRENT_PERIOD', 1)
+CURRENT_YEAR = int(os.getenv('CURRENT_YEAR', datetime.utcnow().year))
+CURRENT_PERIOD = int(os.getenv('CURRENT_PERIOD', (datetime.utcnow().month >= 6) + 1))
+# By default Scrap only Faculdade do Gama (673)
+POSSIBLE_UNITIES = list(map(int, os.getenv('POSSIBLE_UNITIES', '673').split(',')))
+HTML_PATH = os.getenv('HTML_PATH', os.path.join(os.path.dirname(__file__), 'htmls'))
 
 
 class SIGAAScrapper:
-    _url = 'https://sig.unb.br/sigaa/public/turmas/listar.jsf'
-    _cookies: RequestsCookieJar = None
+    _url = 'https://sigaa.unb.br/sigaa/public/turmas/listar.jsf'
+    _cookies: RequestsCookieJar | None = None
 
     @classmethod
     def list_all_disciplines(cls):
@@ -29,8 +33,28 @@ class SIGAAScrapper:
         return cls.__list_disciplines_by_soup(soup)
 
     @classmethod
-    def __create_soup_for(cls, unity: int, year: str,
-                          period: str, level: str = 'G') -> BeautifulSoup:
+    def __create_soup_for(cls, unity: int, year: int,
+                          period: int, level: str = 'G') -> BeautifulSoup:
+        html = cls.__get_html_from_file(unity=unity, year=year, period=period, level=level)
+        if not html:
+            html = cls.__get_html_website(unity=unity, year=year, period=period, level=level)
+            with open(get_html_filename(unity=unity, year=year, period=period, level=level), 'w', encoding='utf-8') as f:
+                f.write(html)
+
+        return BeautifulSoup(html, 'html.parser')
+
+    @classmethod
+    def __get_html_from_file(cls, unity: int, year: int,
+                             period: int, level: str) -> str:
+        try:
+            with open(get_html_filename(unity=unity, year=year, period=period, level=level), 'r', encoding='utf-8') as f:
+                return f.read()
+        except FileNotFoundError:
+            return ''
+
+    @classmethod
+    def __get_html_website(cls, unity: int, year: int,
+                          period: int, level: str) -> str:
         if not cls._cookies:
             cls._cookies = requests.get(cls._url).cookies
 
@@ -44,7 +68,8 @@ class SIGAAScrapper:
 
         response = requests.post(cls._url, data=request_data,
                                  cookies=cls._cookies)
-        return BeautifulSoup(response.content, 'html.parser')
+
+        return response.text
 
     @classmethod
     def __list_disciplines_by_soup(cls,
@@ -86,6 +111,9 @@ class SIGAAScrapper:
 
     @classmethod
     def list_unities(cls) -> list[int]:
+        if POSSIBLE_UNITIES:
+            return POSSIBLE_UNITIES
+
         response = requests.get(cls._url)
         if not cls._cookies:
             cls._cookies = response.cookies
@@ -122,3 +150,9 @@ class SIGAAScrapper:
 
 def string_cleanup(text: str) -> str:
     return re.sub(r'\s+', ' ', text).strip()
+
+
+def get_html_filename(**kwargs) -> str:
+    os.makedirs(HTML_PATH, exist_ok=True)
+    return os.path.join(HTML_PATH, '{level}-{unity}-{year}-{period}.html'.format(**kwargs))
+
