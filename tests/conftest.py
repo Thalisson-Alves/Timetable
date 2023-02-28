c54916a53790b5aa0422a4d7350a1b70e1b8900a
Thalisson-Alves <thalisson2030@gmail.com>
2023-02-28 20:25:13 -0300

    restructure conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,7 @@ from utils.entities.offer import Offer
 from utils.entities.schedule import Schedule, Time
 
 
-next_id_to_use = 1
-def create_discipline(*schedules: Schedule):
-    global next_id_to_use
-
-    id_ = next_id_to_use
-    next_id_to_use += 1
+def create_discipline(id_: int, *schedules: Schedule):
     return Discipline.with_offers(
         id_=f'D{id_}',
         name=f'Discipline {id_}',
@@ -28,51 +23,52 @@ def create_discipline(*schedules: Schedule):
     )
 
 
-@fixture(scope='session')
+@fixture
 def disciplines_no_collision() -> list[Discipline]:
     return [
-        create_discipline(Schedule(days=[1, 3, 5],
-                                   arrival=Time.from_string('10:00'),
-                                   departure=Time.from_string('11:50'))),
-        create_discipline(Schedule(days=[1, 2, 3],
-                                   arrival=Time.from_string('08:00'),
-                                   departure=Time.from_string('09:50'))),
-        create_discipline(Schedule(days=[2, 4, 5],
-                                   arrival=Time.from_string('14:00'),
-                                   departure=Time.from_string('15:50'))),
+        create_discipline(1, Schedule(days=[1, 3, 5],
+                                      arrival=Time.from_string('10:00'),
+                                      departure=Time.from_string('11:50'))),
+        create_discipline(2, Schedule(days=[1, 2, 3],
+                                      arrival=Time.from_string('08:00'),
+                                      departure=Time.from_string('09:50'))),
+        create_discipline(3, Schedule(days=[2, 4, 5],
+                                      arrival=Time.from_string('14:00'),
+                                      departure=Time.from_string('15:50'))),
     ]
 
 
-@fixture(scope='session')
+@fixture
 def disciplines_with_1_collision() -> list[Discipline]:
     return [
-        create_discipline(Schedule(days=[1, 3, 5],
-                                   arrival=Time.from_string('10:00'),
-                                   departure=Time.from_string('11:50'))),
-        create_discipline(Schedule(days=[2, 4, 5],
-                                   arrival=Time.from_string('10:00'),
-                                   departure=Time.from_string('11:50'))),
+        create_discipline(4, Schedule(days=[1, 3, 5],
+                                      arrival=Time.from_string('10:00'),
+                                      departure=Time.from_string('11:50'))),
+        create_discipline(5, Schedule(days=[2, 4, 5],
+                                      arrival=Time.from_string('10:00'),
+                                      departure=Time.from_string('11:50'))),
     ]
 
 
-@fixture(scope='session')
+@fixture
 def disciplines_with_2_collisions(disciplines_with_1_collision: list[Discipline]) -> list[Discipline]:
     return [
         *disciplines_with_1_collision,
-        create_discipline(Schedule(days=[4, 5],
-                                   arrival=Time.from_string('08:00'),
-                                   departure=Time.from_string('11:50'))),
+        create_discipline(6, Schedule(days=[4, 5],
+                                      arrival=Time.from_string('08:00'),
+                                      departure=Time.from_string('11:50'))),
     ]
 
-@fixture(scope='session')
+
+@fixture
 def disciplines_with_3_collisions(disciplines_with_2_collisions: list[Discipline]) -> list[Discipline]:
     return [
         *disciplines_with_2_collisions,
-        create_discipline(Schedule(days=[3, 5],
-                                   arrival=Time.from_string('10:00'),
-                                   departure=Time.from_string('13:50'))),
-        create_discipline(Schedule(days=[3, 5],
-                                   arrival=Time.from_string('12:00'),
-                                   departure=Time.from_string('13:50'))),
+        create_discipline(7, Schedule(days=[3, 5],
+                                      arrival=Time.from_string('10:00'),
+                                      departure=Time.from_string('13:50'))),
+        create_discipline(8, Schedule(days=[3, 5],
+                                      arrival=Time.from_string('12:00'),
+                                      departure=Time.from_string('13:50'))),
     ]
 
