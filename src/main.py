d7651c496cf040320edc65653ca60245f922da1b
Thalisson-Alves <thalisson2030@gmail.com>
2023-03-28 20:47:52 -0300

    :poop: Watch for vacancy

--- src/main.py
+++ src/main.py
@@ -10,6 +10,18 @@ from views import markdown_view
 def main(args: argparse.Namespace):
     ScrapperDisciplineRepository.init(SIGAAScrapper())
 
+    # TODO - Factor this ??
+    #      - I guess it would be better if this kind of thing
+    #        was a subcommand instead of an argument
+    if args.watch:
+        discipline = discipline_for_name(args.watch)
+        if not discipline:
+            print('Discipline not found. Try again.')
+            return
+
+        watch_discipline(discipline.id_)
+        return
+
     if args.list:
         display_disciplines()
 
@@ -21,6 +33,35 @@ def main(args: argparse.Namespace):
 
     all_timetables = generate_all_timetables(disciplines)
     markdown_view.save_to_file(args.output_file, all_timetables)
+
+
+def watch_discipline(discipline_id: str):
+    import os
+    from time import sleep
+
+    counter = 0
+    while True:
+        try:
+            sleep(30)
+            counter += 1
+
+            ScrapperDisciplineRepository.init(SIGAAScrapper())
+
+            discipline = ScrapperDisciplineRepository.get(discipline_id)
+            if not discipline:
+                continue
+
+            remaining = 0
+            for offers in discipline.offers.values():
+                remaining = sum(o.vacancy_remaining for o in offers)
+
+            print(f'\r{counter} - There are {remaining} remaining vacancies on {discipline.name}', end='', flush=True)
+            if remaining:
+                # TODO - Use a better notifier, maybe send an e-mail or something
+                os.system(f'spd-say "Hurry up! Only {remaining} vacancy in {discipline.name}"')
+                print()
+        except KeyboardInterrupt: break
+        except: ...
 
 
 def display_disciplines():
@@ -77,6 +118,10 @@ def parsed_args() -> argparse.Namespace:
                         help='Output MarkDown file path')
     parser.add_argument('-l', '--list', action='store_true',
                         help='Display all disciplines')
+    # TODO - Add argument to configure the time between requests
+    #      - Add argument to specify which offer to scrap (based on schedule, teacher, ...)
+    parser.add_argument('-w', '--watch', type=str, default='',
+                        help='Discipline to watch for vacancies')
     return parser.parse_args()
 
 
